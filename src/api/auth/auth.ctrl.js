e93ecf508702873948dde000fa9de290babf2c47
Eunho.J <jho6394@hanyang.ac.kr>
2022-01-23 23:35:49 +0900

    receive access token

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 import User from '../../models/user';
 
 export const login = async ctx => {
-  const {access_token} = ctx.body;
+  const {access_token} = ctx.request.body;
   console.log(access_token);
   ctx.body = "gotit!";
   return;
