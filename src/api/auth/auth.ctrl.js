a7a3d24348151b1cd6eb681c1512f327e0314fe4
Eunho.J <jho6394@hanyang.ac.kr>
2022-01-24 00:44:15 +0900

    get info by access token

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -5,7 +5,21 @@ import axios from 'axios';
 export const login = async ctx => {
   const {access_token} = ctx.request.body;
   console.log(access_token);
-
+  await axios({
+    url: "https://kapi.kakao.com/v1/user/updtae_profile",
+    headers: {
+      'Authorization': `Bearer ${access_token}`,
+      'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+    },
+    method: 'POST',
+    data: {
+      properties: {
+        "login_token": "hello"
+      }
+    }
+  }).then(response => {
+    console.log(response.data);
+  })
   await axios({
     url: "https://kapi.kakao.com/v2/user/me",
     headers: {
