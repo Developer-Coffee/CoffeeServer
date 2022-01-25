2309fe3060d78457ec6f1c8c704b13d077d8323e
Eunho.J <jho6394@hanyang.ac.kr>
2022-01-25 21:52:34 +0900

    auth middleware test

--- src/lib/authMiddleware.js
+++ src/lib/authMiddleware.js
@@ -1,7 +1,7 @@
 
 
 const authMiddleware = async (ctx, next) => {
-  const { user } = ctx.query;
+  const { user } = ctx.state;
   if (!user) {
     console.log("no auth with token!");
     ctx.status = 401; //Unauthorized
