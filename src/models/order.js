f40a9d465b9146c90fc7a321c3483859950666a6
Eunho.J <jho6394@hanyang.ac.kr>
2022-01-26 13:28:01 +0900

    board orders test

--- src/models/order.js
+++ src/models/order.js
@@ -86,7 +86,7 @@ OrderSchema.statics.getGroupByMenu = async function(board) {
     for (const item of result) {
       if (item.combinedName === combinedName) {
         for (const orderId of item.orderIds){
-          if (orderId.user.equals(order.user)){
+          if (orderId.user === order.user){
             found = true;
             orderId.count += order.count;
           }
